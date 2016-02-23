af8a739fdd1a67eb9bb99881483c05e2c8d4f693
std <std@myotest.com>
2016-02-23 10:11:32 +0100

    - clean header

--- myotest-prerecorded-data-collection/datacollection.h
+++ myotest-prerecorded-data-collection/datacollection.h
@@ -1,30 +1,30 @@
 /* **************************************************************************
 *
 *
-     __  __             _            _      _____             
-    |  \/  |           | |          | |    / ____|    /\      
-    | \  / |_   _  ___ | |_ ___  ___| |_  | (___     /  \     
-    | |\/| | | | |/ _ \| __/ _ \/ __| __|  \___ \   / /\ \    
-    | |  | | |_| | (_) | ||  __/\__ \ |_   ____) | / ____ \ _ 
+     __  __             _            _      _____
+    |  \/  |           | |          | |    / ____|    /\
+    | \  / |_   _  ___ | |_ ___  ___| |_  | (___     /  \
+    | |\/| | | | |/ _ \| __/ _ \/ __| __|  \___ \   / /\ \
+    | |  | | |_| | (_) | ||  __/\__ \ |_   ____) | / ____ \ _
     |_|  |_|\__, |\___/ \__\___||___/\__| |_____(_)_/    \_(_)
-             __/ |                                            
-            |___/                                             
+             __/ |
+            |___/
 *
 * Copyright (c) 2004-2016 Myotest SA
 *
-* PackageLicenseDeclared: Apache-2.0 
-* 
-* Licensed under the Apache License, Version 2.0 (the "License"); 
-* you may not use this file except in compliance with the License. 
-* You may obtain a copy of the License at 
-* 
-*     http://www.apache.org/licenses/LICENSE-2.0 
-* 
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-* See the License for the specific language governing permissions and 
-* limitations under the License. 
+* PackageLicenseDeclared: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
 ****************************************************************************/
 
 #ifndef DATACOLLECTION_H
@@ -36,21 +36,22 @@
 extern "C" {
 #endif
 
-typedef struct sample_t{
+typedef struct sample_t
+{
     uint64_t timestamp;
     int32_t x;
     int32_t y;
     int32_t z;
 } sample_t;
 
-typedef struct rawdata_t{
+typedef struct rawdata_t
+{
     const sample_t* data;
     uint32_t length;
     uint32_t tickHz;
     uint8_t gRange;
     uint8_t adcBits;
 } rawdata_t;
-
 
 //not yet implemented
 void datacollection_convertRawTicks2Ms(sample_t* sample, uint32_t tickHz);
--- source/datacollection.c
+++ source/datacollection.c
@@ -1,51 +1,53 @@
 /* **************************************************************************
 *
 *
-     __  __             _            _      _____             
-    |  \/  |           | |          | |    / ____|    /\      
-    | \  / |_   _  ___ | |_ ___  ___| |_  | (___     /  \     
-    | |\/| | | | |/ _ \| __/ _ \/ __| __|  \___ \   / /\ \    
-    | |  | | |_| | (_) | ||  __/\__ \ |_   ____) | / ____ \ _ 
+     __  __             _            _      _____
+    |  \/  |           | |          | |    / ____|    /\
+    | \  / |_   _  ___ | |_ ___  ___| |_  | (___     /  \
+    | |\/| | | | |/ _ \| __/ _ \/ __| __|  \___ \   / /\ \
+    | |  | | |_| | (_) | ||  __/\__ \ |_   ____) | / ____ \ _
     |_|  |_|\__, |\___/ \__\___||___/\__| |_____(_)_/    \_(_)
-             __/ |                                            
-            |___/                                             
+             __/ |
+            |___/
 *
 * Copyright (c) 2004-2016 Myotest SA
 *
-* PackageLicenseDeclared: Apache-2.0 
-* 
-* Licensed under the Apache License, Version 2.0 (the "License"); 
-* you may not use this file except in compliance with the License. 
-* You may obtain a copy of the License at 
-* 
-*     http://www.apache.org/licenses/LICENSE-2.0 
-* 
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-* See the License for the specific language governing permissions and 
-* limitations under the License. 
+* PackageLicenseDeclared: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
 ****************************************************************************/
 
 #include "myotest-prerecorded-data-collection/datacollection.h"
 #include <math.h>
 
-void datacollection_convertRawTicks2Ms(sample_t* sample, uint32_t tickHz){
+void datacollection_convertRawTicks2Ms(sample_t* sample, uint32_t tickHz)
+{
     uint64_t ticks = sample->timestamp;
     ticks = round(ticks/((float) tickHz / 1000));
     sample->timestamp = ticks;
 }
 
-void datacollection_convertRawAcc2Mg(sample_t* sample, uint8_t gRange, uint8_t adcBits){
-	//not yet implemented
-	(void) sample;
-	(void) gRange;
-	(void) adcBits;
+void datacollection_convertRawAcc2Mg(sample_t* sample, uint8_t gRange, uint8_t adcBits)
+{
+    //not yet implemented
+    (void) sample;
+    (void) gRange;
+    (void) adcBits;
 }
 
 void datacollection_convertTiTag2Mg(sample_t *sample)
-	{
-	sample->x = round((float)sample->x * 1000.0f / 32.0f);
-	sample->y = round((float)sample->y * 1000.0f / 32.0f);
-	sample->z = round((float)sample->z * 1000.0f / 32.0f);
+{
+    sample->x = round((float)sample->x * 1000.0f / 32.0f);
+    sample->y = round((float)sample->y * 1000.0f / 32.0f);
+    sample->z = round((float)sample->z * 1000.0f / 32.0f);
 }
